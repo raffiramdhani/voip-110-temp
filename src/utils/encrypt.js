5d76ab804c3a90f7ad7f2edbff6e752929ec0fce
vsept <verzchseptian@gmail.com>
2023-03-13 14:36:52 +0700

    update encrypt aes256 cbc

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -5,19 +5,17 @@ export const decrypt = (val, iv = null, key = null) => {
   try {
     const IV = iv ?? env.VITE_APP_DECODE_IV;
     const KEY = key ?? env.VITE_APP_DECODE_KEY;
-    var encrypted = CryptoJS.enc.Base64.parse(val);
+    // var encrypted = CryptoJS.enc.Base64.parse(val);
     var key = CryptoJS.enc.Utf8.parse(KEY);
     var iv = CryptoJS.enc.Utf8.parse(IV);
 
     var decrypted = CryptoJS.AES.decrypt(
-      {
-        ciphertext: encrypted,
-      },
+      val,
       key,
       {
         iv: iv,
-        mode: CryptoJS.mode.CTR,
-        padding: CryptoJS.pad.NoPadding,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7,
       }
     );
     return decrypted.toString(CryptoJS.enc.Utf8);
