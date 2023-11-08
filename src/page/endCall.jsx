7579aff377228b8f6a2b7a384a9652d4bd3e3c3a
fachalik <fachalik@gmail.com>
2023-11-08 15:16:55 +0700

    fix: add hangup postmessage

--- src/page/endCall.jsx
+++ src/page/endCall.jsx
@@ -1,21 +1,22 @@
-import { Grid, Typography } from "@mui/material";
-import React from "react";
-import useRouteStore from "@/store/routeStore";
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
+import useRouteStore from '@/store/routeStore';
 
 export default function endCall() {
   const route = useRouteStore((state) => state);
   React.useEffect(() => {
     setTimeout(() => {
-      route.push("login")
+      window.parent.postMessage('hangup', '*');
+      route.push('login');
     }, 5000);
-  })
+  });
   return (
     <Grid
       container
       spacing={0}
       justifyContent="center"
       alignItems="center"
-      sx={{ minHeight: "100vh", backgroundColor: "white" }}
+      sx={{ minHeight: '100vh', backgroundColor: 'white' }}
     >
       <Typography>Terima kasih telah menghubungi kami.</Typography>
     </Grid>
