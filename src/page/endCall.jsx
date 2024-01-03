eebc5246aedfd8fb988f1c1b668604531b9410a1
chalik <47066050+fachalik@users.noreply.github.com>
2024-01-03 13:43:42 +0700

    Revert "fix: add hangup postmessage"
    This reverts commit 7579aff377228b8f6a2b7a384a9652d4bd3e3c3a.

--- src/page/endCall.jsx
+++ src/page/endCall.jsx
@@ -1,22 +1,21 @@
-import { Grid, Typography } from '@mui/material';
-import React from 'react';
-import useRouteStore from '@/store/routeStore';
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import useRouteStore from "@/store/routeStore";
 
 export default function endCall() {
   const route = useRouteStore((state) => state);
   React.useEffect(() => {
     setTimeout(() => {
-      window.parent.postMessage('hangup', '*');
-      route.push('login');
+      route.push("login")
     }, 5000);
-  });
+  })
   return (
     <Grid
       container
       spacing={0}
       justifyContent="center"
       alignItems="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'white' }}
+      sx={{ minHeight: "100vh", backgroundColor: "white" }}
     >
       <Typography>Terima kasih telah menghubungi kami.</Typography>
     </Grid>
