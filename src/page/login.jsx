25b3e239eb7f07571076679899caee82b0ae882d
Mochhamad Raffi Ramdhani <46930178+mraffiramdhani@users.noreply.github.com>
2024-02-12 17:25:43 +0700

    update writing

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -139,7 +139,7 @@ export default function login(props) {
       }
       // }
     } else {
-      setMsgError("Please solve captcha!");
+      setMsgError("Maaf, Captcha tidak valid.");
     }
     setLoading(false);
   };
