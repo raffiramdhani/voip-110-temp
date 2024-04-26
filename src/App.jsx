3d39c1a225e740a748cf0aa3a3c17bb683606728
fachalik <fachalik@gmail.com>
2024-04-26 23:25:23 +0700

    fix: version 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ export default function App() {
               fontWeight: 'bold',
             }}
           >
-            Version: 1.1
+            Version: 1.2
           </Typography>
         </Box>
       </Fade>
