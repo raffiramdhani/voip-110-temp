e559b44b98338b0658a2c2cf100963844dd7b872
tulus <tulusibrahim20@gmail.com>
2024-05-30 11:46:34 +0700

    fix: update

--- src/page/phoneCall.jsx
+++ src/page/phoneCall.jsx
@@ -338,6 +338,7 @@ export default function phoneCall() {
 
   return (
     <Flex
+      vertical
       // position={`${type === "web" ? "absolute" : ""}`}
       // width={`${type === "web" ? "25%" : "100%"}`}
       // height={`${type === "web" ? "70%" : "100vh"}`}
@@ -347,7 +348,6 @@ export default function phoneCall() {
       // flexDirection="column"
       // boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
       // backgroundColor="white"
-      vertical
     >
       <Flex
         align="center"
