e99fe70c10a69895fd1ffb5cecf82387c7aee0fa
tulus <tulusibrahim20@gmail.com>
2024-06-04 10:51:30 +0700

    fix: update style

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -150,6 +150,7 @@ const Welcome = (props) => {
           }}
         >
           <Flex
+            align="start"
             style={{
               border: "1px solid #C4C4C4",
               borderRadius: "10px",
