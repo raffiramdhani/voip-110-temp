10a5df857728533d665b103ec21141d56760a98d
Mochhamad Raffi Ramdhani <raffi.programming@gmail.com>
2025-03-17 11:56:13 +0700

    fix: fetch only kabupaten name from current location

--- src/utils/utilitys.js
+++ src/utils/utilitys.js
@@ -1,21 +1,21 @@
 export function formatPhoneNumber(phoneNumber) {
   // Check if the number already starts with '+62'
-  if (phoneNumber.startsWith('+62')) {
+  if (phoneNumber.startsWith("+62")) {
     // If it starts with '+62', return the number as is
     return phoneNumber;
   }
 
   // Remove any non-numeric characters from the input
-  const numericOnly = phoneNumber.replace(/\D/g, '');
+  const numericOnly = phoneNumber.replace(/\D/g, "");
 
   // Check if the number starts with '08', '628', or '8'
-  if (numericOnly.startsWith('08')) {
+  if (numericOnly.startsWith("08")) {
     // If it starts with '08', add the country code and return the formatted number
     return `+62${numericOnly.substring(1)}`;
-  } else if (numericOnly.startsWith('628')) {
+  } else if (numericOnly.startsWith("628")) {
     // If it starts with '628', return the formatted number with the country code
     return `+${numericOnly}`;
-  } else if (numericOnly.startsWith('8')) {
+  } else if (numericOnly.startsWith("8")) {
     // If it starts with '8', add the country code and return the formatted number
     return `+62${numericOnly}`;
   } else {
@@ -25,13 +25,9 @@ export function formatPhoneNumber(phoneNumber) {
 }
 
 export function getLocationDetail(address) {
-  if (address.city_district) {
-    return address.city_district.toLowerCase();
-  } else if (address.city) {
-    return address.city.toLowerCase();
-  } else if (address.state) {
-    return address.state.toLowerCase();
+  if (address?.county) {
+    return address?.county?.toLowerCase();
   } else {
-    return 'Location detail not available';
+    return "Location detail not available";
   }
 }
